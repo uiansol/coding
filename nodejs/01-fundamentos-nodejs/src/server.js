1a1c645c9b6245016c218d45ae3aba966d1752bd
Uian Sol <usoldeveloper@gmail.com>
2024-01-24 22:18:56 -0300

    nodejs more with streams

--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = [{
   id: 1,
@@ -6,20 +7,23 @@ const users = [{
   email: 'johndoe@example.com',
 }]
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
-    
+
+  await json(req, res)
+
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
